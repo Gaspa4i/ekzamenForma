1f0675394f58b6b10453510d68139bf346abc0ed
NarTi_000 <NarTiiiNDK@mail.ru>
2020-06-20 09:57:51 +0500

    Добавьте файлы проекта.

---
+++ ekzamen forma/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ekzamen_forma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnSrav = new System.Windows.Forms.Button();
+            this.txt1TrA = new System.Windows.Forms.TextBox();
+            this.txt1TrB = new System.Windows.Forms.TextBox();
+            this.txt1TrC = new System.Windows.Forms.TextBox();
+            this.авторToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.александрЖуковToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txt2TrA = new System.Windows.Forms.TextBox();
+            this.txt2TrB = new System.Windows.Forms.TextBox();
+            this.txt2TrC = new System.Windows.Forms.TextBox();
+            this.lblVivod = new System.Windows.Forms.Label();
+            this.оформлениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фонToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.серыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.чёрныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.белыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.авторToolStripMenuItem,
+            this.оформлениеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(339, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnSrav
+            // 
+            this.btnSrav.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSrav.Location = new System.Drawing.Point(122, 60);
+            this.btnSrav.Name = "btnSrav";
+            this.btnSrav.Size = new System.Drawing.Size(88, 43);
+            this.btnSrav.TabIndex = 1;
+            this.btnSrav.Text = "   сравнить\r\nтреугольники";
+            this.btnSrav.UseVisualStyleBackColor = true;
+            this.btnSrav.Click += new System.EventHandler(this.btnSrav_Click);
+            // 
+            // txt1TrA
+            // 
+            this.txt1TrA.Location = new System.Drawing.Point(15, 44);
+            this.txt1TrA.Name = "txt1TrA";
+            this.txt1TrA.Size = new System.Drawing.Size(75, 20);
+            this.txt1TrA.TabIndex = 2;
+            // 
+            // txt1TrB
+            // 
+            this.txt1TrB.Location = new System.Drawing.Point(15, 70);
+            this.txt1TrB.Name = "txt1TrB";
+            this.txt1TrB.Size = new System.Drawing.Size(75, 20);
+            this.txt1TrB.TabIndex = 2;
+            // 
+            // txt1TrC
+            // 
+            this.txt1TrC.Location = new System.Drawing.Point(15, 96);
+            this.txt1TrC.Name = "txt1TrC";
+            this.txt1TrC.Size = new System.Drawing.Size(75, 20);
+            this.txt1TrC.TabIndex = 2;
+            // 
+            // авторToolStripMenuItem
+            // 
+            this.авторToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.александрЖуковToolStripMenuItem});
+            this.авторToolStripMenuItem.ForeColor = System.Drawing.Color.Aqua;
+            this.авторToolStripMenuItem.Name = "авторToolStripMenuItem";
+            this.авторToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.авторToolStripMenuItem.Text = "автор";
+            // 
+            // александрЖуковToolStripMenuItem
+            // 
+            this.александрЖуковToolStripMenuItem.Name = "александрЖуковToolStripMenuItem";
+            this.александрЖуковToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.александрЖуковToolStripMenuItem.Text = "Александр Жуков";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // txt2TrA
+            // 
+            this.txt2TrA.Location = new System.Drawing.Point(237, 44);
+            this.txt2TrA.Name = "txt2TrA";
+            this.txt2TrA.Size = new System.Drawing.Size(75, 20);
+            this.txt2TrA.TabIndex = 2;
+            // 
+            // txt2TrB
+            // 
+            this.txt2TrB.Location = new System.Drawing.Point(237, 70);
+            this.txt2TrB.Name = "txt2TrB";
+            this.txt2TrB.Size = new System.Drawing.Size(75, 20);
+            this.txt2TrB.TabIndex = 2;
+            // 
+            // txt2TrC
+            // 
+            this.txt2TrC.Location = new System.Drawing.Point(237, 96);
+            this.txt2TrC.Name = "txt2TrC";
+            this.txt2TrC.Size = new System.Drawing.Size(75, 20);
+            this.txt2TrC.TabIndex = 2;
+            // 
+            // lblVivod
+            // 
+            this.lblVivod.AutoSize = true;
+            this.lblVivod.Location = new System.Drawing.Point(12, 150);
+            this.lblVivod.Name = "lblVivod";
+            this.lblVivod.Size = new System.Drawing.Size(0, 13);
+            this.lblVivod.TabIndex = 3;
+            // 
+            // оформлениеToolStripMenuItem
+            // 
+            this.оформлениеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фонToolStripMenuItem});
+            this.оформлениеToolStripMenuItem.Name = "оформлениеToolStripMenuItem";
+            this.оформлениеToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.оформлениеToolStripMenuItem.Text = "оформление";
+            // 
+            // фонToolStripMenuItem
+            // 
+            this.фонToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.серыйToolStripMenuItem,
+            this.чёрныйToolStripMenuItem,
+            this.белыйToolStripMenuItem});
+            this.фонToolStripMenuItem.Name = "фонToolStripMenuItem";
+            this.фонToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.фонToolStripMenuItem.Text = "фон";
+            // 
+            // серыйToolStripMenuItem
+            // 
+            this.серыйToolStripMenuItem.Name = "серыйToolStripMenuItem";
+            this.серыйToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.серыйToolStripMenuItem.Text = "серый";
+            this.серыйToolStripMenuItem.Click += new System.EventHandler(this.серыйToolStripMenuItem_Click);
+            // 
+            // чёрныйToolStripMenuItem
+            // 
+            this.чёрныйToolStripMenuItem.Name = "чёрныйToolStripMenuItem";
+            this.чёрныйToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.чёрныйToolStripMenuItem.Text = "чёрный";
+            this.чёрныйToolStripMenuItem.Click += new System.EventHandler(this.чёрныйToolStripMenuItem_Click);
+            // 
+            // белыйToolStripMenuItem
+            // 
+            this.белыйToolStripMenuItem.Name = "белыйToolStripMenuItem";
+            this.белыйToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.белыйToolStripMenuItem.Text = "белый";
+            this.белыйToolStripMenuItem.Click += new System.EventHandler(this.белыйToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(339, 193);
+            this.Controls.Add(this.lblVivod);
+            this.Controls.Add(this.txt2TrC);
+            this.Controls.Add(this.txt1TrC);
+            this.Controls.Add(this.txt2TrB);
+            this.Controls.Add(this.txt1TrB);
+            this.Controls.Add(this.txt2TrA);
+            this.Controls.Add(this.txt1TrA);
+            this.Controls.Add(this.btnSrav);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Сравнение треугольников";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem авторToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem александрЖуковToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button btnSrav;
+        private System.Windows.Forms.TextBox txt1TrA;
+        private System.Windows.Forms.TextBox txt1TrB;
+        private System.Windows.Forms.TextBox txt1TrC;
+        private System.Windows.Forms.TextBox txt2TrA;
+        private System.Windows.Forms.TextBox txt2TrB;
+        private System.Windows.Forms.TextBox txt2TrC;
+        private System.Windows.Forms.Label lblVivod;
+        private System.Windows.Forms.ToolStripMenuItem оформлениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem фонToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem серыйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem чёрныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem белыйToolStripMenuItem;
+    }
+}
+
---
+++ ekzamen forma/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ekzamen_forma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSrav_Click(object sender, EventArgs e)
+        {
+            double a1 = 0; double b1 = 0; double c1 = 0;
+            double a2 = 0; double b2 = 0; double c2 = 0;
+            double p1 = 0; double s1 = 0;
+            double p2 = 0; double s2 = 0;
+            bool corect = true;
+            try
+            {
+                 a1 = Convert.ToDouble(txt1TrA.Text);  b1 = Convert.ToDouble(txt1TrB.Text);  c1 = Convert.ToDouble(txt1TrC.Text);
+                 a2 = Convert.ToDouble(txt2TrA.Text);  b2 = Convert.ToDouble(txt2TrB.Text);  c2 = Convert.ToDouble(txt2TrC.Text);
+            }
+            catch
+            {
+                lblVivod.Text="Введены некорректные данные";
+                corect = false;
+            }
+            if (corect)
+            {
+                p1 = (a1 + b1 + c1) / 2;
+                p2 = (a2 + b2 + c2) / 2;
+                s1 = Math.Sqrt(p1 * (p1 - a1) * (p1 - b1) * (p1 - c1));
+                s2 = Math.Sqrt(p2 * (p2 - a2) * (p2 - b2) * (p2 - c2));
+
+                if (s1 > s2) lblVivod.Text = "Площадь первого треугольника на " + (s1 - s2).ToString("0.00") + " больше второго";
+                else if (s2 > s1) lblVivod.Text = "Площадь второго треугольника на " + (s2 - s1).ToString("0.00") + " больше первого";
+                else lblVivod.Text = "Площади треугольников равны";
+
+                if (a1 + b1 <= c1 || b1 + c1 <= a1 || c1 + a1 <= b1)
+                    if (a2 + b2 <= c2 || b2 + c2 <= a2 || c2 + a2 <= b2) lblVivod.Text = "Оба треугольника невозможны";
+                    else lblVivod.Text = "Первый треугольник невозможен";
+                else
+                    if (a2 + b2 <= c2 || b2 + c2 <= a2 || c2 + a2 <= b2) lblVivod.Text = "Второй треугольник невозможен";
+            }
+        }
+
+        private void серыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+
+        private void чёрныйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(64, 64, 64);
+        }
+
+        private void белыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(224, 224, 224);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
